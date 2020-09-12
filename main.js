f58e402410a2b607241cceee323fb12f1e8e915b
bringebass <oyvindbringedal@gmail.com>
2020-09-12 19:46:09 +0200

    vdsavsd

---
+++ main.js
@@ -0,0 +1,28 @@
+/* Selectors */
+
+const showDropdownFirstBox = document.querySelector('.showDropdownFirstBox');
+const DropdownFirstBox = document.querySelector('.dropdown-first-box');
+const showDropdownSecondBox = document.querySelector('.showDropdownSecondBox');
+const DropdownSecondBox = document.querySelector('.dropdown-second-box');
+const showDropdownThirdBox = document.querySelector('.showDropdownThirdBox');
+const DropdownThirdBox = document.querySelector('.dropdown-third-box');
+const showDropdownFourthBox = document.querySelector('.showDropdownFourthBox');
+const DropdownFourthBox = document.querySelector('.dropdown-fourth-box');
+const showDropdownFiftBox = document.querySelector('.showDropdownFiftBox');
+const DropdownFiftBox = document.querySelector('.dropdown-fift-box');
+
+/* Show dropdowns */
+
+function showDropdowns (c, b) {
+c.addEventListener("click", () => {
+    b.classList.toggle("show");
+
+}
+)};
+
+showDropdowns (showDropdownFirstBox, DropdownFirstBox);
+showDropdowns (showDropdownSecondBox, DropdownSecondBox);
+showDropdowns (showDropdownThirdBox, DropdownThirdBox);
+showDropdowns (showDropdownFourthBox, DropdownFourthBox);
+showDropdowns (showDropdownFiftBox, DropdownFiftBox);
+
